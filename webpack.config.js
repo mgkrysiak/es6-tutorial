9fbf76a62b5be0599bb8926cb702fe5d69c13dd9
mgkrysiak <gabi.krysiak@gmail.com>
2019-01-12 01:45:05 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+  app: './js/main.js',
+  ratefinder: './js/ratefinder.js'
+},
+output: {
+  path: path.resolve(__dirname, 'build'),
+  filename: '[name].bundle.js'
+},
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+          }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
